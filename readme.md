ee9328a9bf1b3395d10efb26f1f6340b8fe8d0b8
sonked <sonked@hotmail.de>
2024-07-12 21:57:18 +0200

    improve readme

--- readme.md
+++ readme.md
@@ -25,10 +25,10 @@ imports:
 @startuml Demo
 
 [*] --> DemoState: [ CheckAlwaysTrue ]
-[*] -[bold]-> [*]: / Print(The guards needs to be implemented)
+[*] -[bold]-> [*]: / Print(The guard needs to be implemented)
 
 DemoState: do / AddMsg(Hello)
-DemoState: do / AddMsg(World1)
+DemoState: do / AddMsg(World)
 DemoState: do / AddMsg(!)
 DemoState --> BurnState: [ CheckAlwaysTrue ] / Print(Go to BurnState)
 DemoState -[bold]-> [*]
--- src/controller/templates/Sm.java
+++ src/controller/templates/Sm.java
@@ -10,65 +10,6 @@ public class Sm {
 	static Map<String, State> getStates() {
 		Map<String, State> states = new HashMap<>();
 
-		State DemoState= new State();
-
-    StateAction DemoState0Action = new StateAction();
-    DemoState0Action.setAction("AddMsgAction");
-
-    ArrayList<String> DemoState0ActionArgs = new ArrayList<>();
-        DemoState0ActionArgs.add("Hello");
-
-    DemoState0Action.setActionArgs(DemoState0ActionArgs);
-
-    DemoState.getActions().add(DemoState0Action);
-
-    StateAction DemoState1Action = new StateAction();
-    DemoState1Action.setAction("AddMsgAction");
-
-    ArrayList<String> DemoState1ActionArgs = new ArrayList<>();
-        DemoState1ActionArgs.add("World1");
-
-    DemoState1Action.setActionArgs(DemoState1ActionArgs);
-
-    DemoState.getActions().add(DemoState1Action);
-
-    StateAction DemoState2Action = new StateAction();
-    DemoState2Action.setAction("AddMsgAction");
-
-    ArrayList<String> DemoState2ActionArgs = new ArrayList<>();
-        DemoState2ActionArgs.add("!");
-
-    DemoState2Action.setActionArgs(DemoState2ActionArgs);
-
-    DemoState.getActions().add(DemoState2Action);
-
-    Transition DemoState0Transition = new Transition();DemoState0Transition.setGuard("CheckAlwaysTrue");
-
-        ArrayList<String> DemoState0GuardArgs = new ArrayList<>();
-
-        DemoState0Transition.setGuardArgs(DemoState0GuardArgs);DemoState0Transition.setAction("PrintAction");
-
-        ArrayList<String> DemoState0TransActionArgs = new ArrayList<>();
-            DemoState0TransActionArgs.add("Go to BurnState");
-
-        DemoState0Transition.setActionArgs(DemoState0TransActionArgs);DemoState0Transition.setNext("BurnState");DemoState0Transition.setNegation(false);
-    DemoState0Transition.setType(TransitionType.NORMAL);
-    DemoState.getTransitions().add(DemoState0Transition);
-
-    Transition DemoState1Transition = new Transition();DemoState1Transition.setNext("EndState");DemoState1Transition.setNegation(false);
-    DemoState1Transition.setType(TransitionType.HAPPY);
-    DemoState.getTransitions().add(DemoState1Transition);
-
-states.put("DemoState", DemoState);
-
-
-
-		State EndState= new State();
-
-states.put("EndState", EndState);
-
-
-
 		State BurnState= new State();
 
     StateAction BurnState0Action = new StateAction();
@@ -108,15 +49,75 @@ states.put("BurnState", BurnState);
     StartState0Transition.setType(TransitionType.NORMAL);
     StartState.getTransitions().add(StartState0Transition);
 
-    Transition StartState1Transition = new Transition();StartState1Transition.setNext("EndState");StartState1Transition.setNegation(false);
-    StartState1Transition.setType(TransitionType.ERROR);
+    Transition StartState1Transition = new Transition();StartState1Transition.setAction("PrintAction");
+
+        ArrayList<String> StartState1TransActionArgs = new ArrayList<>();
+            StartState1TransActionArgs.add("The guard needs to be implemented");
+
+        StartState1Transition.setActionArgs(StartState1TransActionArgs);StartState1Transition.setNext("EndState");StartState1Transition.setNegation(false);
+    StartState1Transition.setType(TransitionType.HAPPY);
     StartState.getTransitions().add(StartState1Transition);
 
-    Transition StartState2Transition = new Transition();StartState2Transition.setNext("EndState");StartState2Transition.setNegation(false);
-    StartState2Transition.setType(TransitionType.HAPPY);
-    StartState.getTransitions().add(StartState2Transition);
-
 states.put("StartState", StartState);
+
+
+
+		State DemoState= new State();
+
+    StateAction DemoState0Action = new StateAction();
+    DemoState0Action.setAction("AddMsgAction");
+
+    ArrayList<String> DemoState0ActionArgs = new ArrayList<>();
+        DemoState0ActionArgs.add("Hello");
+
+    DemoState0Action.setActionArgs(DemoState0ActionArgs);
+
+    DemoState.getActions().add(DemoState0Action);
+
+    StateAction DemoState1Action = new StateAction();
+    DemoState1Action.setAction("AddMsgAction");
+
+    ArrayList<String> DemoState1ActionArgs = new ArrayList<>();
+        DemoState1ActionArgs.add("World");
+
+    DemoState1Action.setActionArgs(DemoState1ActionArgs);
+
+    DemoState.getActions().add(DemoState1Action);
+
+    StateAction DemoState2Action = new StateAction();
+    DemoState2Action.setAction("AddMsgAction");
+
+    ArrayList<String> DemoState2ActionArgs = new ArrayList<>();
+        DemoState2ActionArgs.add("!");
+
+    DemoState2Action.setActionArgs(DemoState2ActionArgs);
+
+    DemoState.getActions().add(DemoState2Action);
+
+    Transition DemoState0Transition = new Transition();DemoState0Transition.setGuard("CheckAlwaysTrue");
+
+        ArrayList<String> DemoState0GuardArgs = new ArrayList<>();
+
+        DemoState0Transition.setGuardArgs(DemoState0GuardArgs);DemoState0Transition.setAction("PrintAction");
+
+        ArrayList<String> DemoState0TransActionArgs = new ArrayList<>();
+            DemoState0TransActionArgs.add("Go to BurnState");
+
+        DemoState0Transition.setActionArgs(DemoState0TransActionArgs);DemoState0Transition.setNext("BurnState");DemoState0Transition.setNegation(false);
+    DemoState0Transition.setType(TransitionType.NORMAL);
+    DemoState.getTransitions().add(DemoState0Transition);
+
+    Transition DemoState1Transition = new Transition();DemoState1Transition.setNext("EndState");DemoState1Transition.setNegation(false);
+    DemoState1Transition.setType(TransitionType.HAPPY);
+    DemoState.getTransitions().add(DemoState1Transition);
+
+states.put("DemoState", DemoState);
+
+
+
+		State EndState= new State();
+
+states.put("EndState", EndState);
 
 
 
